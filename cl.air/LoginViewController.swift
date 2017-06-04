aa9db94228aa52c2fcb5b2b00c6a3294634bba87
Serg T <sergtsaeb@gmail.com>
2017-06-03 17:43:36 -0700

    Initial commit

---
+++ cl.air/LoginViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  cl.air
+//
+//  Created by Sergelenbaatar Tsogtbaatar on 6/3/17.
+//  Copyright © 2017 Serg Tsogtbaatar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+       
+    }
+    
+    
+
+
+}
+
